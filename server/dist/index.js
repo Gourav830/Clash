a076cf8fb089835a18b8ee558baabcdd329dc8b5
gourav830 <gouravsingla830@gmail.com>
2024-11-20 17:34:57 +0530

    Implement email rendering and sending functionality; add welcome, verify, and password reset email templates; update .gitignore to exclude environment files and build artifacts

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import "dotenv/config";
 import path from 'path';
 import { fileURLToPath } from 'url';
+import ejs from 'ejs';
+import { sendEmail } from './config/mail.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,9 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'views'));
-app.get('/', (req, res) => {
-    return res.render('welcome');
-    // res.send("Hello World");
+app.get('/', async (req, res) => {
+    const html = await ejs.renderFile(path.resolve(__dirname, 'views/emails/welcome.ejs'), { name: "Rahul" });
+    // return res.render('emails/welcome',{name:"Rahul"});
+    await sendEmail("gefib48763@exoular.com", "testing", html);
+    res.send("Hello World");
 });
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
