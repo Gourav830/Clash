7469033df5231347260dcc5263785469458dc151
gourav830 <gouravsingla830@gmail.com>
2024-11-21 01:51:05 +0530

    Add Redis email queue integration; update environment variables and implement email job processing

---
+++ server/src/jobs/emailJobs.ts
@@ -0,0 +1,24 @@
+import { Job, Queue,RedisConnection,Worker } from "bullmq";
+import { defaultQueueOtions, redisConnectionOptions } from "../config/queue.js";
+import { sendEmail } from "../config/mail.js";
+
+export const emailQueueName = "emailQueue"
+interface EmailJobDataType{
+    to:string;
+    subject:string;
+    body:string;
+}
+export const emailQueue = new Queue(emailQueueName, {
+    connection: redisConnectionOptions,
+    defaultJobOptions:defaultQueueOtions,
+    });
+
+
+export const queueWorker = new Worker(emailQueueName, async (job:Job)=> {
+        const data = job.data;
+        await sendEmail(data.to,data.subject,data.body);
+        console.log("This is job data by bull mq" ,data);
+
+},{
+    connection: redisConnectionOptions,
+})
