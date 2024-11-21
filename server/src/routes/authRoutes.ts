bedddd66ac418047e523b01d8c9845fc215ed83b
gourav830 <gouravsingla830@gmail.com>
2024-11-21 10:02:47 +0530

    Add user registration with email uniqueness check and password encryption

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -2,6 +2,8 @@ import { Router, Request, Response } from "express";
 import { registerSchema } from "../validations/authValidations.js";
 import { ZodError } from "zod";
 import { formatEror } from "../helper.js";
+import prisma from "../config/database.js";
+import bcrypt from "bcrypt";
 const router = Router();
 //REgister Route
 
@@ -10,7 +12,32 @@ router.post("/register", async (req: Request, res: Response) => {
   try {
     const body = req.body
     const payload = registerSchema.parse(body)
-    res.json(payload)
+    let user = await prisma.user.findUnique(({where:{email:payload.email}}))
+    if(user){
+       res.status(422).json({
+        errors:{
+          email:"Email already exists"
+        }
+      })
+    }
+
+    //Encryption
+
+    const salt = await bcrypt.genSalt(13);
+      payload.password = await bcrypt.hashSync(payload.password,salt);
+    
+      //database
+      await prisma.user.create({
+        data:{
+          name:payload.name,
+          email:payload.email,
+          password:payload.password
+        }
+      })
+      res.json({message:"User registered successfully"})
+
+
+
   } 
   catch (error) {
     if(error instanceof ZodError){
@@ -19,7 +46,7 @@ router.post("/register", async (req: Request, res: Response) => {
     
     }
     console.log(error);
-    res.status(500).json(error);
+    res.status(500).json({message:"Something went wrong . Please try again later"});
   }
 });
 
