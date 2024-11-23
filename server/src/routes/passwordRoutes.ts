ae32852651dc4770d1c1fa2b17409d1b68585865
gourav830 <gouravsingla830@gmail.com>
2024-11-24 00:08:38 +0530

    Add password reset functionality: implement routes for forgot and reset password, enhance validation schemas, and integrate email notifications

--- server/src/routes/passwordRoutes.ts
+++ server/src/routes/passwordRoutes.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from "express";
 import { ZodError } from "zod";
-import { formatEror } from "../helper.js";
-import { ForgertPasswordSchema } from "../validations/passwordvalidation.js";
+import { formatEror, renderEmailEjs } from "../helper.js";
+import { ForgertPasswordSchema, passwordResetSchema } from "../validations/passwordvalidation.js";
 import bcrypt from 'bcrypt';
 import {v4 as uuid4} from 'uuid';
 import prisma from "../config/database.js";
+import { emailQueue, emailQueueName } from "../jobs/emailJobs.js";
 
 const router = Router();
 
@@ -31,6 +32,16 @@ router.post("/forgot-password", async (req: Request, res: Response) => {
                     email:payload.email
                 }
             })
+            const url = `${process.env.APP_URL}/reset-password?email=${payload.email}&token=${token}`;
+            const html = await renderEmailEjs("forgot-password", {
+                url:url
+            });
+            await emailQueue.add(emailQueueName, {
+                to: payload.email,
+                subject: "Reset Password",
+                body: html,
+            });
+
 
                  return res.json({ message: "Email has been sent" });
      } catch (error) {
@@ -45,6 +56,47 @@ router.post("/forgot-password", async (req: Request, res: Response) => {
           }
     });
 
+    router.post("/reset-password", async (req: Request, res: Response) => {
+        try {
+            const body = req.body;
+            const payload = passwordResetSchema.parse(body);
+            const user = await prisma.user.findUnique({where:{email:payload.email}})
+            if(!user || user==null)  {
+                return res.status(422).json({message:"Email not found",errors:{
+                    email:"Email not found"
+                }})
+            } 
+            const salt = await bcrypt.genSalt(10);
+            const hashedToken = await bcrypt.hash(payload.token, salt);
+            if(hashedToken !== user.passwordResetToken){
+                return res.status(422).json({message:"Invalid token",errors:{
+                    token:"Invalid token"
+                }})
+            }
+            const newSalt = await bcrypt.genSalt(10);
+            const newPassword = await bcrypt.hash(payload.password, newSalt);
+            await prisma.user.update({
+                data:{
+                    password:newPassword,
+                    passwordResetToken:null,
+                    token_send_at:null
+                },
+                where:{
+                    email:payload.email
+                }
+            })
+            return res.json({message:"Password has been reset"});
+        } catch (error) {
+            if (error instanceof ZodError ) {
+              const errors = formatEror(error);
+              return res.status(422).json({ message: "Invalid data", errors });
+            }
+            console.error(error);
+            return res
+              .status(500)
+              .json({ message: "Something went wrong. Please try again later." });
+          }
+    });
 
 
 export default router;
--- server/src/validations/passwordvalidation.ts
+++ server/src/validations/passwordvalidation.ts
@@ -2,3 +2,11 @@ import { z } from "zod";
 export const ForgertPasswordSchema = z.object({
     email: z.string({message:"Enter  Email"}).email({message:"Enter valid Email"}),
 });
+export const passwordResetSchema = z.object({    
+   
+    email: z.string({message:"Enter  Email"}).email({message:"Enter valid Email"}),
+    password: z.string({message:"Enter Password"}).min(6,{message:"Password must be at least 6 characters long"}),
+    confirm_password: z.string({message:"Enter Password"}).min(6),
+  }).refine((data) => data.password === data.confirm_password, {
+    message: "Passwords do not match",path:["confirm_password"]
+  });
