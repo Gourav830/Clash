ee73e624898b2e2fc5c4448a1de0f54b901df26d
gourav830 <gouravsingla830@gmail.com>
2024-11-27 01:35:54 +0530

    Enhance clash route error handling: implement Zod validation and improve error responses

--- server/src/routes/clashRoutes.ts
+++ server/src/routes/clashRoutes.ts
@@ -2,19 +2,31 @@
 
 
 import { Router ,Request,Response} from "express";
+import { ZodError } from "zod";
+import { formatEror } from "../helper.js";
+import { clashSchema } from "../validations/clashValidation.js";
 
 const router = Router()
 
 router.post('/' , async (req:Request,res:Response) => {
-    res.send('Hello World')
-
-
+    
+    
     try{
-
-    }catch(err){
-        console.log(err)
+        // res.send('Hello World')
+        const body = req.body
+        const payload = clashSchema.parse(body)
         
-    }
+
+    }catch (error) {
+        if (error instanceof ZodError) {
+          const errors = formatEror(error);
+          return res.status(422).json({ message: "Invalid data", errors });
+        }
+        console.error(error);
+        return res
+          .status(500)
+          .json({ message: "Something went wrong. Please try again later." });
+      }
 })
 
 
