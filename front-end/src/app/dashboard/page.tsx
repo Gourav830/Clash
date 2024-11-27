df6d1a5e3f715e77c598973d34076a6e7056ef20
gourav830 <gouravsingla830@gmail.com>
2024-11-27 16:10:23 +0530

    Add dashboard page and navigation components: implement NavBar, UserAvatar, and LogoutModal; configure middleware for dashboard route

---
+++ front-end/src/app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBar from '../../components/base/navBar';
+
+const Page = () => {
+    return (
+        <div className='container'>
+            <NavBar />
+            dashboard
+        </div>
+    );
+}
+
+export default Page;
---
+++ front-end/src/components/base/navBar.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import React, { useState } from 'react';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+  } from "@/components/ui/dropdown-menu"
+  import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import UserAvtar from '../common/userAvtar';
+import LogoutModal from '../auth/logoutModal';
+
+const NavBar = () => {
+    const [open,setOpen] = useState(false);
+    return (
+        <>
+<LogoutModal open={open} setOpen={setOpen} />
+        <nav className='flex justify-between items-center h-14 p-2 w-full'>
+            
+            <h1 className=" text-4xl font-extrabold bg-gradient-to-r from-pink-400 to bg-purple-500 text-transparent bg-clip-text">
+          Clash
+        </h1>
+        <DropdownMenu>
+  <DropdownMenuTrigger>< UserAvtar/></DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem onClick={()=>setOpen(true)} >Logout</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+
+
+        </nav>
+        </>
+    
+    );
+}
+
+export default NavBar;
